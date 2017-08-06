756a46c51b637291493ac9c32549d4a5fe9c7143
jemapplecasey <casey.lee.black@gmail.com>
2017-08-06 04:13:40 +0000

    Implamented fixes for immages and grid for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -56,12 +56,8 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    # Perform the lookup
-
-    # Destroy/delete the record
     @portfolio_item.destroy
 
-    # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
